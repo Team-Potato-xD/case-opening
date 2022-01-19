9c53223c33b85c616f1872bc0e1ab0624cd85eb3
19carina99 <carinabm@hotmail.no>
2022-01-19 15:57:07 +0100

    Create items.js
    test items

---
+++ items.js
@@ -0,0 +1,36 @@
+// const itemId = new IncrementableIndex('items');
+
+function createItem(name, rarity, description, price){
+    model.items.push({
+        id: 1,
+        image: '',
+        name: name,
+        rarity: rarity,
+        description: description,
+        price: price,
+    });
+}
+
+function editId(){
+}
+
+function editImage(){
+}
+
+function editName(){
+}
+
+function editDescription(){
+}
+
+function editPrice(){
+}
+
+//edit things from 2nd function
+
+function deleteItem(id){
+}
+
+function findItem(id){
+}
+//hehe
