9b6921ab0c015dfdb22045a3596dafe5e83e0343
Bozsimitsu <bozsiftw6@gmail.com>
2022-01-19 16:39:22 +0100

    Create case-functions.js

---
+++ case-functions.js
@@ -0,0 +1,17 @@
+
+
+function createCase(name, description, price, drops) {
+    model.cases.push({
+        id: 1,
+        image: '',
+        name: name,
+        description: description,
+        price: price,
+        drops: drops,
+
+    });
+}
+
+function addItemToCase(caseId, itemId) {
+
+};
